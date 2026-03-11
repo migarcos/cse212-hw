ccf98239636d981469e71412edb69a07938f7583
migarcos <migark@hotmail.com>
2026-03-11 18:11:16 -0500

    learn week 02

--- week02/learn/Program.cs
+++ week02/learn/Program.cs
@@ -1,7 +1,7 @@
-﻿// Console.WriteLine("\n======================\nSimple Queue\n======================");
-// SimpleQueue.Run();
+﻿Console.WriteLine("\n======================\nSimple Queue\n======================");
+SimpleQueue.Run();
 
-SimpleStack.Run();
+// SimpleStack.Run();
 
 // Uncomment and run as you get to the solution part
 // Console.WriteLine("\n======================\nSimple Queue\n======================");
