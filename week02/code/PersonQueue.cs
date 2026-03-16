ccfa1c6207e8947d98dfff314f2fa256f4a0d7f9
migarcos <migark@hotmail.com>
2026-03-16 12:00:54 -0500

    Taking turns solved

---
+++ week02/code/PersonQueue.cs
@@ -0,0 +1,37 @@
+/// <summary>
+/// A basic implementation of a Queue
+/// </summary>
+public class PersonQueue
+{
+    private readonly List<Person> _queue = new();
+
+    public int Length => _queue.Count;
+
+    /// <summary>
+    /// Add a person to the queue
+    /// </summary>
+    /// <param name="person">The person to add</param>
+    public void Enqueue(Person person)
+    {
+        // error adding new element using Insert
+        // _queue.Insert(0, person);
+        _queue.Add(person);
+    }
+
+    public Person Dequeue()
+    {
+        var person = _queue[0];
+        _queue.RemoveAt(0);
+        return person;
+    }
+
+    public bool IsEmpty()
+    {
+        return Length == 0;
+    }
+
+    public override string ToString()
+    {
+        return $"[{string.Join(", ", _queue)}]";
+    }
+}
