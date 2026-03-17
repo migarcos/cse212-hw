3f4a8e8c9644e10a3a9a9a49c9bc21d6de935503
migarcos <migark@hotmail.com>
2026-03-17 11:47:50 -0500

    week 03 learning

---
+++ week03/learn/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\n======================\nDuplicate Counter\n======================");
+        DuplicateCounter.Run();
+        DuplicateCounterSolution.Run();
+
+        // Console.WriteLine("\n======================\nTranslator\n======================");
+        // Translator.Run();
+    }
+}
