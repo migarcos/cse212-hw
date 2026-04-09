6452e9f14fe1ef42e720492f775784f588296de7
migarcos <migark@hotmail.com>
2026-04-08 21:43:39 -0500

    code

---
+++ week06/code/Node.cs
@@ -0,0 +1,47 @@
+public class Node
+{
+    public int Data { get; set; }
+    public Node? Right { get; private set; }
+    public Node? Left { get; private set; }
+
+    public Node(int data)
+    {
+        this.Data = data;
+    }
+
+    public void Insert(int value)
+    {
+        // TODO Start Problem 1 - only allow unique values to be added to the tree
+        if (value == Data)
+            return;
+            
+        if (value < Data)
+        {
+            // Insert to the left
+            if (Left is null)
+                Left = new Node(value);
+            else
+                Left.Insert(value);
+        }
+        else
+        {
+            // Insert to the right
+            if (Right is null)
+                Right = new Node(value);
+            else
+                Right.Insert(value);
+        }
+    }
+
+    public bool Contains(int value)
+    {
+        // TODO Start Problem 2
+        return false;
+    }
+
+    public int GetHeight()
+    {
+        // TODO Start Problem 4
+        return 0; // Replace this line with the correct return statement(s)
+    }
+}
